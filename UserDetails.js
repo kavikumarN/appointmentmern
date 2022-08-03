442572d7e1c9859cbe9e0f3f861e9c4502c70761
Kavikumar N <msg4kavi@gmail.com>
2022-08-03 16:45:55 +0530

    Update UserDetails.js

--- UserDetails.js
+++ UserDetails.js
@@ -1,12 +1,23 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const UserSchema = mongoose.Schema({
-    Name: String,
-    FullName: String,
-    Address: String,
-    PhoneNumber: String,
-    Email_Id: String,
-})
 
-export default mongoose.model("UserDetails",UserSchema)
+var UserSchema = mongoose.Schema({
+    // Sid: { type : String , unique : false, required : false },
+    Name: { type : String , unique : false, required : true },
+    Phone: { type : String , unique : true, required : true },
+    Email: { type : String , unique : false, required : false },
+    Time: { type : String , unique : false, required : true },
+    Notes: { type : String , unique : false, required : false },
+    Cr_at: { type : String , unique : false, required : true },
+});
 
+// UserSchema.pre('save', function(next) {
+//     var doc = this;
+//     counter.findByIdAndUpdate({_id: 'Entity_id'}, {$inc: { seq: -1} }, function(error, counter)   {
+//         if(error)
+//             return next(error);
+//         doc.testvalue = counter.seq;
+//         next();
+//     });
+// });
+ export default mongoose.model("UserDetails",UserSchema)
